3f221f3037dedaf3a32bb9b374532266db5d7832
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-28 04:11:59 +0500

    Add samples of payload from tutorial

--- slide/urls.py
+++ slide/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:slug>', views.show, name='show'),
 ]
--- slide/views.py
+++ slide/views.py
@@ -1,7 +1,14 @@
 # from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
+from .models import Slideshow
 
 def index(request):
-    return HttpResponse('Coming soon')
+    latest_slideshow_list = Slideshow.objects.order_by('-date_published')[:5]
+    output = ', '.join([s.name for s in latest_slideshow_list])
+    return HttpResponse(output)
+
+def show(request, slug):
+    s = Slideshow.objects.filter(slug=slug).first()
+    output = f'Slideshow {s.name}' if s else f'No slideshow found at {slug}'
+    return HttpResponse(output)
