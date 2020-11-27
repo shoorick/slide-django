e23ec8c0d357148571d2fcf4e5b5f1c6d1f4955c
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-28 03:41:51 +0500

    Add method Slideshow.was_published_recently from tutorial

--- slide/models.py
+++ slide/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Slideshow(models.Model):
     name = models.CharField(max_length=255)
@@ -10,3 +12,6 @@ class Slideshow(models.Model):
 
     def __str__(self):
         return self.name
+
+    def was_published_recently(self):
+        return self.date_published >= timezone.now() - datetime.timedelta(days=1)
