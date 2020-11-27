ac003e2b1d431892206c1b106baa75c454f3876f
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-28 04:19:53 +0500

    Use template to print list of slideshows

--- slide/views.py
+++ slide/views.py
@@ -1,12 +1,12 @@
-# from django.shortcuts import render
-
+from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Slideshow
 
 def index(request):
     latest_slideshow_list = Slideshow.objects.order_by('-date_published')[:5]
-    output = ', '.join([s.name for s in latest_slideshow_list])
-    return HttpResponse(output)
+    context = {'latest_slideshow_list': latest_slideshow_list}
+    return render(request, 'slide/index.html', context)
 
 def show(request, slug):
     s = Slideshow.objects.filter(slug=slug).first()
