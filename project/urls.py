09249a348b050f3197cbe15d81916f80b1f95842
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-28 05:44:56 +0500

    Add temporary redirect from / to /slide

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import redirect_to_slide_index
+
 urlpatterns = [
+    path('', redirect_to_slide_index),
     path('slide/', include('slide.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ project/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import redirect
+
+def redirect_to_slide_index(request):
+    return redirect('/slide/')
