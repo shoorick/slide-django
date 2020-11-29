b1f564bce9305b7d157e072c776c6a2cff71c71f
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-30 02:44:56 +0500

    Add test for HTTP GET requests to application

--- slide/tests.py
+++ slide/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from .models import Profile, Slideshow
 
@@ -9,3 +9,24 @@ class ProfileModelTest(TestCase):
 class SlideshowModelTest(TestCase):
     def test_slideshow_has_str(self):
         self.assertIn('__str__', dir(Slideshow), msg='Slideshow has __str__')
+
+class GetTest(TestCase):
+    def test_root_redirected_to_slide_list(self):
+        c = Client()
+        res = c.get('/')
+        self.assertEqual(res.status_code, 302, msg='HTTP 302 Moved')
+        self.assertEqual(res.url, '/slide/', msg='Redirected to slide list')
+
+    def test_slide_list(self):
+        c = Client()
+        res = c.get('/slide/')
+        self.assertEqual(res.status_code, 200, msg='HTTP 200 OK')
+        self.assertTrue(res.has_header('Content-Type'), msg='Has Content-Type')
+        self.assertEqual(res.charset, 'utf-8', msg='UTF-8')
+        self.assertGreater(len(res.content), 0)
+
+    def test_nonexistent(self):
+        c = Client()
+        res = c.get('/nonexistent')
+        self.assertEqual(res.status_code, 404, msg='HTTP 404 Not Found')
+
