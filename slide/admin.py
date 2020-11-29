fd7399e9f1b355682877bca00f7b9ccdf29ccf51
Alexander Sapozhnikov <shoorick@cpan.org>
2020-11-30 00:00:18 +0500

    Add user profiles and owner to slideshows

--- slide/admin.py
+++ slide/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Slideshow
+from .models import Slideshow, Profile
 
 admin.site.register(Slideshow)
+admin.site.register(Profile)
--- slide/models.py
+++ slide/models.py
@@ -1,17 +1,38 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Slideshow(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=63, unique=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     date_published = models.DateTimeField(default=timezone.now)
     is_published = models.BooleanField(default=True)
     source = models.TextField(verbose_name='Source code')
-    options = models.JSONField(null=True)
+    options = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     def was_published_recently(self):
         return self.date_published >= timezone.now() - datetime.timedelta(days=1)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField(max_length=1024, blank=True)
+    options = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return ' '.join((self.user.first_name, self.user.last_name))
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
