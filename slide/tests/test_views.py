5486c8bc5bac3e6f63ca4daa1e963191aa91f28a
Alexander Sapozhnikov <shoorick@cpan.org>
2020-12-07 22:39:54 +0500

    Split united tests.py into tests/test_*.py files

--- slide/tests/test_views.py
+++ slide/tests/test_views.py
@@ -1,11 +1,5 @@
 from django.test import TestCase, Client
 
-from .models import Profile, Slideshow
-
-class ProfileModelTest(TestCase):
-    def test_profile_has_str(self):
-        self.assertIn('__str__', dir(Profile), msg='Profile has __str__')
-
 class SlideshowModelTest(TestCase):
     def test_slideshow_has_str(self):
         self.assertIn('__str__', dir(Slideshow), msg='Slideshow has __str__')
