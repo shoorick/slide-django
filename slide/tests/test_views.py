bae72e43f103b4fbc402de0626c46bc53b56a8b3
Alexander Sapozhnikov <shoorick@cpan.org>
2020-12-08 01:06:59 +0500

    Add GetTest.setUp method to create common client for all other methods

--- slide/tests/test_views.py
+++ slide/tests/test_views.py
@@ -1,21 +1,22 @@
 from django.test import TestCase, Client
 
 class GetTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
     def test_root_redirected_to_slide_list(self):
-        c = Client()
-        res = c.get('/')
+        res = self.client.get('/')
         self.assertEqual(res.status_code, 302, msg='HTTP 302 Moved')
         self.assertEqual(res.url, '/slide/', msg='Redirected to slide list')
 
     def test_slide_list(self):
-        c = Client()
-        res = c.get('/slide/')
+        res = self.client.get('/slide/')
         self.assertEqual(res.status_code, 200, msg='HTTP 200 OK')
         self.assertTrue(res.has_header('Content-Type'), msg='Has Content-Type')
         self.assertEqual(res.charset, 'utf-8', msg='UTF-8')
         self.assertGreater(len(res.content), 0)
 
     def test_nonexistent(self):
-        c = Client()
-        res = c.get('/nonexistent')
+        res = self.client.get('/nonexistent')
         self.assertEqual(res.status_code, 404, msg='HTTP 404 Not Found')
